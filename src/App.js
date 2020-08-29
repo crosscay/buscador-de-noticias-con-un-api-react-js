872e69455c206795ff0d60679e4c4b18a92453f9
crosscay <caicedocarlos0306@gmail.com>
2020-08-29 18:39:21 -0500

    consultando la api y leyendo los resultados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
@@ -6,6 +6,18 @@ function App() {
 
   // definir la categoria y noticias
   const [ categoria, guardarCartegoria ] = useState(''); 
+  const [ noticias, guardarNoticias ] = useState([]); 
+
+  useEffect(() => {
+    const vacio = '';
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ve&category=${categoria}&apiKey=44e8506694a3469c87570192c669f8db`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles); 
+    }
+    consultarApi();
+  }, [categoria]);
   return (
     <Fragment>
       <Header 
