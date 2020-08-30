624c5886ff874454579fded0594ec38546333511
crosscay <caicedocarlos0306@gmail.com>
2020-08-29 19:20:39 -0500

    mostrando las noticias

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -10,7 +10,7 @@ const Formulario = ({guardarCartegoria}) => {
         { value: 'entertainment', label: 'Entretenimiento' },
         { value: 'health', label: 'Salud' },
         { value: 'science', label: 'Ciencia' },
-        { value: 'sports', label: 'Deporates' },
+        { value: 'sports', label: 'Deportes' },
         { value: 'technology', label: 'Tegnologia' }
     ]
 
--- src/components/ListadoNoticias.js
+++ src/components/ListadoNoticias.js
@@ -4,9 +4,10 @@ import Noticia from './Noticia';
 const ListadoNoticias = ({noticias}) => {
     return ( 
         <div className="row">
-            {noticias.map(noticia =>(
+            {noticias.map(noticia => (
                 <Noticia 
-                    
+                    key={noticia.url}
+                    noticia={noticia}
                 />
             ))};
         </div>
